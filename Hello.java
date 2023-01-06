1f661a3cd0b172a746a797125063e8a7ff5c4e2d
Sudhanshu13091989 <sudhanshucust@gmail.com>
2023-01-06 17:15:59 +0530

    Automatically Build

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ class Hello{
 		System.out.println("HELLO Java Program Using JENKIN");
 		System.out.println("First Perfect Build for Me using java ");
 		System.out.println("Let Play The Jenkin");
+		System.out.println("My MOney Now this will be buid automatically");
 		
 		
 	}
